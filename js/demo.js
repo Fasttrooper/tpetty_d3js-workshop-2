2f8e6c6b911a2ca3c3331cff4491d5a0992d25c7
yellstales <yrysbek@umich.edu>
2026-02-26 10:19:08 -0500

    add materials for workshop 2

---
+++ js/demo.js
@@ -0,0 +1,7 @@
+// import data
+
+// copy over the chart definition from Observable: https://observablehq.com/@d3/arc-diagram
+
+// create initial chart from the data 
+
+// add event listener to the dropdown menu to update the chart when a new option is selected
